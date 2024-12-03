b09f90592dcb1215105134d0e9e6c4be26e7861e
aleksey3sotni <belkov.alex2005@yandex.ru>
2024-12-03 15:08:55 +0300

    Update modCipher.h

--- 4_2/modCipher.h
+++ 4_2/modCipher.h
@@ -1,14 +1,16 @@
 /**
-* @file modAlphaCipher.h
-* @author Беликов А.М.
-* @version 1.0
-* @brief Описание класса modAlphaCipher
-* @date 02.12.2024
-*/
+ * @file modAlphaCipher.h
+ * @author Беликов А.М.
+ * @version 1.0
+ * @brief Описание класса modAlphaCipher и связанных функций.
+ * @date 02.12.2024
+ */
+
 #pragma once
+
 #include <string>
 #include <stdexcept>
-#include <vector>
+
 /**
  * @brief Исключение для ошибок шифрования.
  */
@@ -20,28 +22,47 @@ public:
      */
     explicit cipher_error(const std::string& what_arg) : std::runtime_error(what_arg) {}
 };
+
 /**
  * @brief Подготавливает текст для шифрования.
+ * 
+ * Эта функция удаляет все неалфавитные символы и приводит текст к нижнему регистру,
+ * чтобы обеспечить корректное шифрование.
+ *
  * @param text Исходный текст.
  * @return Подготовленный текст.
  */
 std::string prepareText(const std::string& text);
+
 /**
  * @brief Шифрует текст.
+ * 
+ * Эта функция применяет шифр с использованием заданного ключа для преобразования текста.
+ *
  * @param key Ключ шифрования.
  * @param text Текст для шифрования.
  * @return Зашифрованный текст.
  */
 std::string coder(int key, const std::string& text);
+
 /**
  * @brief Дешифрует текст.
+ * 
+ * Эта функция восстанавливает исходный текст из зашифрованного текста с использованием 
+ * заданного ключа.
+ *
  * @param key Ключ дешифрования.
  * @param text Зашифрованный текст.
  * @return Расшифрованный текст.
  */
 std::string decoder(int key, const std::string& text);
+
 /**
  * @brief Проверяет корректность ключа.
+ *
+ * Эта функция проверяет, является ли ключ допустимым для использования в шифровании,
+ * и при необходимости корректирует его.
+ *
  * @param key Ключ.
  * @param text Текст.
  * @return Корректный ключ.
